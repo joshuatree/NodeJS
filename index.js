8814911f36b07f6175a9d82c371ec56d37478775
Martin Mühl <martin.muehl@gmail.com>
2020-06-01 23:52:49 +0200

    Initial setup
    Create your node.js server file index.js and add the code needed to serve the right page according to the url.

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http
+	.createServer((req, res) => {
+		const filePath = url.parse(req.url, true).pathname === '/' ? 'index' : url.parse(req.url, true).pathname;
+		fs.readFile(`./${filePath}.php`, (err, file) => {
+			if (err) {
+				res.writeHead(404, { 'Content-Type': 'text/html' });
+				return fs.readFile('./404.php', (err, file) => {
+					if (err) {
+						throw err;
+					} else {
+						res.write(file);
+						return res.end();
+					}
+				});
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(file);
+				return res.end();
+			}
+		});
+	})
+	.listen(8080);
